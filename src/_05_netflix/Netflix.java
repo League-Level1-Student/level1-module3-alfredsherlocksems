3eda97b0919d8c2e7754c249175dbf2b8039ebfe
student <student@ip-172-31-52-196.us-west-2.compute.internal>
2021-03-08 13:29:37 -0800

    Finished the neflix class.

--- src/_05_netflix/Netflix.java
+++ src/_05_netflix/Netflix.java
@@ -5,11 +5,23 @@ public class Netflix {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Movie LOR = new Movie("Lord of the Rings", 5);
-		Movie NannyMcPhee = new Movie ("Nanny McPhee", 5);
-		Movie SOM = new Movie("Sound of Music", 5);
-		Movie MaryPoppins = new Movie("Mary Poppins", 5);
-		Movie SOR = new Movie("School of Rock", 5);
+		Movie LOR = new Movie("Lord of the Rings", 10);
+		Movie NannyMcPhee = new Movie ("Nanny McPhee", 9);
+		Movie SOM = new Movie("Sound of Music", 10);
+		Movie MaryPoppins = new Movie("Mary Poppins", 10);
+		Movie SOR = new Movie("School of Rock", 8);
+		LOR.getTicketPrice();
+		NetflixQueue netflix = new NetflixQueue();
+		netflix.addMovie(LOR);
+		netflix.addMovie(NannyMcPhee);
+		netflix.addMovie(SOM);
+		netflix.addMovie(MaryPoppins);
+		netflix.addMovie(SOR);
+		netflix.printMovies();
+		netflix.sortMoviesByRating();
+		System.out.println("The best movie is " + netflix.getBestMovie());
+		System.out.println("The second best movie is " + netflix.getMovie(1));
+		
 	}
 
 }
---
+++ src/_06_gridworld/gridworld_runner.java
@@ -0,0 +1,12 @@
+package _06_gridworld;
+
+import info.gridworld.world.World;
+
+public class gridworld_runner {
+
+	public static void main(String[] args) {
+		World world = new World();
+		
+		
+	}
+}
